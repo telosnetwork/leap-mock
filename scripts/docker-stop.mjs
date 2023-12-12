c983ed2ae6f061046e7e8fa16229e9706de7c0fa
Guillermo Rodriguez <guillermo@telos.net>
2023-12-11 22:21:37 -0300

    Improve naming of npm docker comands also add docker-stop

--- scripts/docker-stop.mjs
+++ scripts/docker-stop.mjs
@@ -1,24 +1,7 @@
-import { fileURLToPath } from 'node:url';
-import path from 'path';
-import fs from "fs";
 import {spawn} from "child_process";
 
-const currentDir = path.dirname(fileURLToPath(import.meta.url));
-const packageJsonFile = path.join(currentDir, '../package.json');
-const packageInfo = JSON.parse(fs.readFileSync(packageJsonFile, 'utf-8'));
-
 // Spawn the process
-const process = spawn(
-    'docker',
-    [
-        'run',
-        '-d',  // detach
-        '--rm',  // remove after use
-        '--network=host',
-        `--name=leap-mock`,
-        `guilledk/leap-mock:${packageInfo.version}`
-    ]
-);
+const process = spawn('docker', ['kill', '--signal=SIGINT', 'leap-mock']);
 
 // Handle standard output
 process.stdout.on('data', (data) => {
